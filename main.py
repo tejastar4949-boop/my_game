369ba703a5614de34f16c96a66cf6a7ec1df102f
tejastar4949-boop <tejastar4949@gmail.com>
2026-04-21 00:18:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,47 @@
-from pyscript import sync
+from pyscript import sync, display
+
+# This tells the website the game has started
+display("", target="loading-msg")
 
 def run_game():
-    # These pop-ups are triggered by the sync library
     try:
+        # These will appear as pop-up boxes at the top of the browser
         name1 = sync.input("Enter the first name:")
         name2 = sync.input("Enter the second name:")
 
-        if name1 and name2:
-            n1 = name1.lower().replace(" ", "")
-            n2 = name2.lower().replace(" ", "")
+        if not name1 or not name2:
+            print("You must enter two names!")
+            return
 
-            list1, list2 = list(n1), list(n2)
-            for char in list1[:]:
-                if char in list2:
-                    list1.remove(char)
-                    list2.remove(char)
+        n1 = name1.lower().replace(" ", "")
+        n2 = name2.lower().replace(" ", "")
 
-            count = len(list1) + len(list2)
-            flames = ["Friends", "Lovers", "Affection", "Marriage", "Enemies", "Siblings"]
+        list1, list2 = list(n1), list(n2)
+        for char in list1[:]:
+            if char in list2:
+                list1.remove(char)
+                list2.remove(char)
 
-            if count > 0:
-                while len(flames) > 1:
-                    split_index = (count % len(flames)) - 1
-                    if split_index >= 0:
-                        flames = flames[split_index + 1:] + flames[:split_index]
-                    else:
-                        flames.pop()
+        count = len(list1) + len(list2)
+        flames = ["Friends", "Lovers", "Affection", "Marriage", "Enemies", "Siblings"]
 
-                print(f"🔥 Result for {name1} & {name2}: {flames[0]} 🔥")
-            else:
-                print("The names are too similar to calculate!")
+        if count > 0:
+            while len(flames) > 1:
+                split_index = (count % len(flames)) - 1
+                if split_index >= 0:
+                    flames = flames[split_index + 1:] + flames[:split_index]
+                else:
+                    flames.pop()
+            
+            # This result will print onto the webpage itself
+            print(f"--- RESULT ---")
+            print(f"Names: {name1} & {name2}")
+            print(f"Status: {flames[0]}")
         else:
-            print("Please enter both names.")
+            print("The names are too similar to calculate!")
+            
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"Something went wrong: {e}")
 
-# IMPORTANT: This line calls the function
+# Start the game
 run_game()
