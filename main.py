a56a61552e3d647a9f06f8e8189f5d33e7888d5e
tejastar4949-boop <tejastar4949@gmail.com>
2026-04-21 00:05:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,39 @@
 from pyscript import sync
 
 def run_game():
-    # 'sync.input' is the secret to making the pop-ups work!
-    name1 = sync.input("Enter the first name:")
-    name2 = sync.input("Enter the second name:")
+    # These pop-ups are triggered by the sync library
+    try:
+        name1 = sync.input("Enter the first name:")
+        name2 = sync.input("Enter the second name:")
 
-    if name1 and name2:
-        n1 = name1.lower().replace(" ", "")
-        n2 = name2.lower().replace(" ", "")
+        if name1 and name2:
+            n1 = name1.lower().replace(" ", "")
+            n2 = name2.lower().replace(" ", "")
 
-        list1, list2 = list(n1), list(n2)
-        for char in list1[:]:
-            if char in list2:
-                list1.remove(char)
-                list2.remove(char)
+            list1, list2 = list(n1), list(n2)
+            for char in list1[:]:
+                if char in list2:
+                    list1.remove(char)
+                    list2.remove(char)
 
-        count = len(list1) + len(list2)
-        flames = ["Friends", "Lovers", "Affection", "Marriage", "Enemies", "Siblings"]
+            count = len(list1) + len(list2)
+            flames = ["Friends", "Lovers", "Affection", "Marriage", "Enemies", "Siblings"]
 
-        if count > 0:
-            while len(flames) > 1:
-                split_index = (count % len(flames)) - 1
-                if split_index >= 0:
-                    flames = flames[split_index + 1:] + flames[:split_index]
-                else:
-                    flames.pop()
+            if count > 0:
+                while len(flames) > 1:
+                    split_index = (count % len(flames)) - 1
+                    if split_index >= 0:
+                        flames = flames[split_index + 1:] + flames[:split_index]
+                    else:
+                        flames.pop()
 
-            print(f"🔥 Result for {name1} & {name2}: {flames[0]} 🔥")
+                print(f"🔥 Result for {name1} & {name2}: {flames[0]} 🔥")
+            else:
+                print("The names are too similar to calculate!")
         else:
-            print("The names are too similar to calculate!")
-    else:
-        print("Please enter both names.")
+            print("Please enter both names.")
+    except Exception as e:
+        print(f"Error: {e}")
 
-# This line actually starts the code
+# IMPORTANT: This line calls the function
 run_game()
